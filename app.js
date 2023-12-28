0ee0893b134e7cb53856d05be79713537922b0a5
eliasyeme <eliasyeme@gmail.com>
2023-12-28 19:20:08 +0300

    Add basic html layout

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello')
+import menuArray from "./data.js"
+
+function getMenuListHtml(menuList) {
+    let menuHtml = ''
+    menuList.forEach(menu => {
+        const {emoji, id, ingredients, name, price} = menu
+        menuHtml += `
+            <article data-id="${id}">
+                <span>${emoji}</span>
+                <div>
+                    <h2>${name}</h2>
+                    <p>${ingredients.join(',')}</p>
+                    <h3>$${price}</h3>
+                </div>
+                <button>+</button>
+            </article>
+        `
+    })
+    return menuHtml
+}
+
+function render() {
+    document.getElementById('app').innerHTML = getMenuListHtml(menuArray)
+}
+
+render()
